084905ff19a91660429bd490ff00c0491f73a5d2
michelange <michelange.anton@gmail.com>
2018-04-17 14:32:05 +0200

    Service asynchrone avec Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,10 @@
-import {Injectable} from '@angular/core';
-import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
+import {Injectable} from "@angular/core";
+import {Hero} from "./hero";
+import {HEROES} from "./mock-heroes";
+import {Observable} from "rxjs/Observable";
+import {of} from "rxjs/observable/of";
+import { delay} from "rxjs/operators";
+import { pipe } from 'rxjs/util/pipe';
 
 @Injectable()
 export class HeroService {
@@ -8,7 +12,10 @@ export class HeroService {
   constructor() {
   }
 
-  getHeroes():Hero[] {
-    return HEROES;
+  getHeroes():Observable<Hero[]> {
+    // Code normal
+    // return of(HEROES);
+    // Avec 3 secondes d'attentes
+    return of(HEROES).pipe(delay(3000));
   }
 }
