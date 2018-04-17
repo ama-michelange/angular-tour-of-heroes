5f825c002fc6ee67e48ff8627f283d941d688e07
michelange <michelange.anton@gmail.com>
2018-04-17 17:51:12 +0200

    Ajout de la mise à jour d'un Héros

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,10 +8,15 @@ import {pipe} from 'rxjs/util/pipe';
 import {MessageService} from "./message.service";
 import {catchError, map, tap} from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 @Injectable()
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
+
 
   constructor(private http:HttpClient, private messageService:MessageService) {
   }
@@ -29,6 +34,14 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero:Hero):Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
