fd24a8d78434080a2f1540f1521705a55e339037
michelange <michelange.anton@gmail.com>
2018-04-17 13:55:52 +0200

    Ajout du provider pour le service Héro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
+import {HeroesComponent} from "./heroes/heroes.component";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
+import {HeroService} from "./hero.service";
 
 
 @NgModule({
@@ -17,7 +17,12 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
