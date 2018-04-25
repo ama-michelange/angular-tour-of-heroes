b3d970625d2d9dee1e7849b2f871d805419ea58d
ama.michelange <ama-michelange@users.noreply.github.com>
2018-04-25 10:45:56 +0200

    Proxy de secours par GP : correction Options / Authorization

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const request = require('request')
+const request = require('request');
+
 const app = express()
 
 // Port of the service on the local server
@@ -8,14 +9,16 @@ const port = 3000;
 // Add CORS Headers to response
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
 // Proxy all requests to external server
 app.all('*', function (req, res, next) {
 console.log('Proxying the request ...' + req.method + ' ' + req.url);
-var x = request({'uri':'http://v2.prixbd-alices.fr/v1/public/api' + req.url, 'method': req.method, 'headers': req.headers});
+
+var x = request({'uri':'http://v2.prixbd-alices.fr/v1/public/api' + req.url, 'method': req.method});
 req.pipe(x);
 x.pipe(res);
 });
